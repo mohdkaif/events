8bc799c183ccbb018b6dc37383e74a9bf7626892
mohdkaif <mohdkaif984@gmail.com>
2022-04-09 15:05:30 +0530

    changes

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -9,8 +9,9 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Event extends Model
 {
     use HasFactory, SoftDeletes;
-    
+
     public $timestamps = true;
+    
     protected $fillable = [
         'title',
         'start',
