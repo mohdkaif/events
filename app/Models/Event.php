c7c535283b47a03481ca1224a8ca1a9bce5cc6f1
s000-simreka <shiva.kumar@simreka.com>
2022-04-09 15:02:49 +0530

    chnages

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Event extends Model
 {
     use HasFactory, SoftDeletes;
+    
     public $timestamps = true;
     protected $fillable = [
         'title',
